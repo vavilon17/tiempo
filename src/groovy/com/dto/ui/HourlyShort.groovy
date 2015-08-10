d74f3b597827a4eded1180408a78c8f9783599b6
keyser soze <vit_3@ukr.net>
2015-08-10 08:21:43 +0300

    show basic weather info

---
+++ src/groovy/com/dto/ui/HourlyShort.groovy
@@ -0,0 +1,12 @@
+package com.dto.ui
+
+import com.tiempo.WeatherType
+
+/**
+ * Created by vit on 26.07.2015.
+ */
+class HourlyShort {
+    String dayTime
+    int temperature
+    WeatherType weatherType
+}
---
+++ src/java/com/tiempo/WeatherType.java
@@ -0,0 +1,18 @@
+package com.tiempo;
+
+/**
+ * Created by vit on 26.07.2015.
+ */
+public enum WeatherType {
+    SUN,
+    SUN_CLOUD,
+    SUN_CLOUD_RAIN,
+    CLOUD,
+    CLOUD_RAIN,
+    CLOUD_BIG_RAIN,
+    CLOUD_LIGHTNING,
+    SNOW,
+    MOON,
+    MOON_CLOUD,
+    MOON_CLOUD_RAIN
+}
